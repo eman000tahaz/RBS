7273234178932502b01fa385bfc1ed229ccbf653
eman000tahaz <emey_gif@yahoo.com>
2017-09-06 11:41:29 +0000

    Solve server conflicts

---
+++ odoo-server/custom/addons/ryada_school/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Ryada School demo',
+    'version': '1.0.1',
+    'category': 'School',
+    'sequence': 20,
+    'summary': 'Ryada School demo',
+    'depends': [
+        'school',
+        'school_attendance',
+        'school_evaluation',
+        'school_event',
+        'school_fees',
+        'school_transport',
+        'timetable',
+        'exam',
+    ],
+    'data': [],
+    'demo': [
+    ],
+    'installable': True,
+    'application': True,
+    'Author': "Eman Taha"
+}
