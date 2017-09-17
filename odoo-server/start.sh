ad58cf6836310d6f0f2e9a6c90be94d9fa2defad
eman000tahaz <emey_gif@yahoo.com>
2017-09-17 14:56:06 +0200

    Solve pc conflicts

--- odoo-server/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-sudo -u odoo /odoo/odoo-server/openerp-server --config=/etc/odoo-server.conf
-#!/bin/sh
-sudo -u odoo /odoo/odoo-server/openerp-server --config=/etc/odoo-server.conf
-#!/bin/sh
-sudo -u odoo /odoo/odoo-server/openerp-server --config=/etc/odoo-server.conf
-#!/bin/sh
-sudo -u odoo /odoo/odoo-server/openerp-server --config=/etc/odoo-server.conf
