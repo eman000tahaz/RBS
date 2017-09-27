072d465a4ac6db21084d7c12939a12e05e7ed291
eman000tahaz <emey_gif@yahoo.com>
2017-09-27 11:13:21 +0200

    [ADD] asset addons

---
+++ custom/addons/rbs_assets/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Ryada Assets',
+    'version': '1.0.1',
+    'category': 'Other',
+    'sequence': 20,
+    'summary': 'Ryada Assets',
+    'depends': [
+        'asset',
+        'mro',
+        'maintenance',
+    ],
+    'data': [
+        'views/asset_views.xml',
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'application': True,
+    'Author': "Eman Taha"
+}
