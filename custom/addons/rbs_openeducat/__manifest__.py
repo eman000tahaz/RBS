b6eca36d15e62e0765a39dc14654c3e24bb4101e
Eman taha <emeytahaz@gmail.com>
2017-10-08 11:13:00 +0200

    [ADD] openeducat

---
+++ custom/addons/rbs_openeducat/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'RBS OpenEduCat',
+    'version': '10.0.3.0.0',
+    'category': 'Education',
+    "sequence": 3,
+    'summary': 'Manage Students, Faculties and Education Institute',
+    'complexity': "easy",
+    'author': 'Eman Taha',
+    'depends': ['openeducat_activity', 'openeducat_admission',
+                'openeducat_assignment', 'openeducat_attendance',
+                'openeducat_exam', 'openeducat_library',
+                'openeducat_parent',
+                'openeducat_timetable'],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'images': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
